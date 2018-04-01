ebaac19e3657fd6d1ecf232f498516a4db8f7b54
Konstantin <sergeef.kos@gmail.com>
2018-04-01 09:07:46 +0300

    base version game

---
+++ main.js
@@ -0,0 +1,194 @@
+'use strict';
+
+let field = document.getElementById('field');
+let table = document.createElement('table');
+let resGoal = document.querySelector('#resultGoal > span');
+let goal;
+let countGoal = 0;
+let speedSnake = 100;
+
+
+for (let i = 0; i < 30; i++) {
+    let tr = table.insertRow(i);
+    
+    for (let y = 0; y < 30; y++) {
+        let td = tr.insertCell(y);
+    }
+}
+
+// append Table in Field 
+field.appendChild(table);
+
+// Create goal color red
+function runGoal() {
+    if (goal != undefined) {
+        goal.classList.remove('goal');
+    }
+    let ranGoal = Math.floor(Math.random() * 900);
+    let choiceRow, choiceColl;
+    
+    if (ranGoal > 30) {
+        choiceRow = parseInt(ranGoal / 30);
+        choiceColl = ranGoal - (choiceRow * 30);
+        goal = table.firstChild.childNodes[choiceRow].childNodes[choiceColl-1];
+    } else {
+        goal = table.firstChild.childNodes[0].childNodes[ranGoal-1];
+    }
+    goal.classList.add('goal');
+}
+
+runGoal();
+
+// snake
+let lenghtSnake = 3;
+let snake = table.firstChild.childNodes[4].childNodes[4];
+
+let bodySnake = [];
+
+snake.classList.add('snake');
+
+
+let verPos = 4;
+let horPos = 4; 
+let runSnakeDown;
+let runSnakeUp;
+let runSnakeRight;
+let runSnakeLeft;
+
+document.addEventListener('keyup', hitKey, false);
+function hitKey(e) {
+    let keyName  = e.key;
+    
+    if(keyName == 'ArrowUp') {
+        snakeUp();
+    }
+    if(keyName == 'ArrowDown') {
+        snakeDown();
+    }
+    if(keyName == 'ArrowRight') {
+        snakeRight();
+    }
+    if(keyName == 'ArrowLeft') {
+        snakeLeft();
+    }
+}
+
+// Run Snake Down
+function snakeDown() {
+    clearInterval(runSnakeUp);
+    clearInterval(runSnakeRight);
+    clearInterval(runSnakeLeft);
+    runSnakeDown = setInterval( () => {
+        console.log('Snake Down');
+        
+        horPos += 1;
+        if (horPos > 29)  {
+            horPos = 29;
+            verifAspect();
+        }
+        
+        snake.classList.remove('snake');
+        snake = table.firstChild.childNodes[horPos].childNodes[verPos];
+        createBodySnake();
+        verif();
+        snake.classList.add('snake');
+    }, speedSnake);
+}
+
+// Run Snake Up
+function snakeUp() {
+    clearInterval(runSnakeDown);
+    clearInterval(runSnakeRight);
+    clearInterval(runSnakeLeft);
+    runSnakeUp = setInterval( () => {
+        console.log('Snake Up');
+        
+        horPos -= 1;
+        if (horPos < 0)  {
+            horPos = 0;
+            verifAspect();
+        }
+        snake.classList.remove('snake');
+        snake = table.firstChild.childNodes[horPos].childNodes[verPos];
+        verif();
+        snake.classList.add('snake');
+    }, speedSnake);
+}
+
+// Run Snake Right
+function snakeRight() {
+    clearInterval(runSnakeUp);
+    clearInterval(runSnakeDown);
+    clearInterval(runSnakeLeft);
+    runSnakeRight = setInterval( () => {
+        console.log('Snake Right');
+        
+        verPos += 1;
+        if (verPos > 29)  {
+            verPos = 29;
+            verifAspect();
+        }
+        snake.classList.remove('snake');
+        snake = table.firstChild.childNodes[horPos].childNodes[verPos];
+        verif();
+        snake.classList.add('snake');
+    }, speedSnake); 
+}
+
+// Run Snake Left
+function snakeLeft() {
+    clearInterval(runSnakeUp);
+    clearInterval(runSnakeDown);
+    clearInterval(runSnakeRight);
+    runSnakeLeft = setInterval( () => {
+        console.log('Snake Left');
+        verPos -= 1;
+        if (verPos < 0)  {
+            verPos = 0;
+            verifAspect();
+        }
+        snake.classList.remove('snake');
+        snake = table.firstChild.childNodes[horPos].childNodes[verPos];
+        verif();
+        snake.classList.add('snake');
+    }, speedSnake);
+}
+// Create Body Snake
+function createBodySnake() {
+    if (bodySnake[lenghtSnake-1]) {
+        bodySnake[lenghtSnake-1].style.background = '#ccc';
+    }
+    for (let x = 0; x < lenghtSnake; x++) {
+        if (x == 0) {
+            bodySnake[0] = snake.parentElement.previousElementSibling.children[4];
+        } else {
+            bodySnake[x] = bodySnake[x-1].parentElement.previousElementSibling.children[4];
+        }
+        bodySnake[x].style.background = '#fff';    
+    }
+}
+
+// verification goal
+function verif() {
+    if (snake == goal) {
+        countGoal += 1;
+        resGoal.textContent = countGoal;
+        runGoal();
+    }
+}
+// verification aspect
+function verifAspect() {
+    let end = document.createElement('span');
+    end.textContent = 'Game Over';
+    table.appendChild(end);
+    end.style.display = 'block';
+    document.removeEventListener('keyup', hitKey, false);
+    clearInterval(runSnakeUp);
+    clearInterval(runSnakeDown);
+    clearInterval(runSnakeRight);
+    clearInterval(runSnakeLeft);
+}
+
+snakeDown();
+
+
